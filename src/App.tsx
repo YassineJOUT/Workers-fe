576a93ed6801524ca50520679b0231ea6cc3e85f
Yassine JOUT <yassinejout@gmail.com>
2019-10-24 22:59:11 +0100

    feat(ResetPassword): Add reset password UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import LoginPage from './components/LoginPage';
 import ForgottenPassord from './components/ForgottenPassword';
+import ResetPassword from './components/ResetPassword';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-        <ForgottenPassord />
+        <ResetPassword />
     </div>
   );
 }
