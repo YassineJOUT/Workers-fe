b46fce883b72e4750c9431bd256b83a507eff5d9
Yassine JOUT <yassinejout@gmail.com>
2019-10-24 22:55:36 +0100

    feat(ForgottenPassword): Add password forgotten component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoginPage from './components/LoginPage';
+import ForgottenPassord from './components/ForgottenPassword';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-        <LoginPage />
+        <ForgottenPassord />
     </div>
   );
 }
--- src/components/ForgottenPassword.tsx
+++ src/components/ForgottenPassword.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Form,Button,Row,Col, Container} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope,faLock } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 const LoginPage : React.FC = () => {
     return (
@@ -20,31 +20,14 @@ const LoginPage : React.FC = () => {
                             
                             
                         </Form.Group>
-                        <Form.Group  className='mt-4' controlId="formBasicPassword">
-                            <span className='pl-3 pt-1 position-absolute' >
-                                <FontAwesomeIcon icon={faLock} />
-                            </span>
-                            <Form.Control size="sm"  className="pl-5"  type="password" placeholder="Password" />
-                            
-                            
-                        </Form.Group>
-                        <Row >
-                            <Col >
-                                <Form.Group  controlId="formBasicCheckbox">
-                                    <Form.Check   type="checkbox" label="Remember me" />
-                                </Form.Group>
-                            </Col>
-                            <Col >
-                           
-                            <a href="#" className='float-sm-right' >Forgot password</a>
-                            </Col>
-                        </Row>
+                        
+                        
                         <Button size="sm"  className='mb-3 btn btn-primary btn-lg btn-block' variant="primary" type="submit">
-                            Log in
+                            Recover password
                         </Button>
                         <br />
                         
-                        Or <a href="#">Register now</a>
+                        Or <a href="#">Back to login!</a>
                     </Form>
                 </Col>
             </Row>
