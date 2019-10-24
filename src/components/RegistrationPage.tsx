01a0dbf1d83c8360c3e4e683746c48258e76a647
Yassine JOUT <yassinejout@gmail.com>
2019-10-24 22:30:07 +0100

    feat(Registration): FC registration component

---
+++ src/components/RegistrationPage.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {Form,Button,Row,Col, Container} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope,faUser,faLock } from '@fortawesome/free-solid-svg-icons'
+
+const RegistrationPage : React.FC = () => {
+    return (
+        <div>
+            <Container>
+
+            <Row style={{ marginTop : '100px'}}>
+                <Col md={{ span: 3, offset: 4 }}>
+                    <Form>
+                        <Form.Group className='mt-4' controlId="formBasicText">
+                             <span className='pl-3 pt-1 position-absolute' >
+                                <FontAwesomeIcon icon={faUser} />
+                            </span>
+                            <Form.Control size="sm" className="pl-5" type="text" placeholder="Username" />
+                            
+                            
+                        </Form.Group>
+                        <Form.Group  className='mt-4' controlId="formBasicEmail">
+                            <span className='pl-3 pt-1 position-absolute' >
+                                <FontAwesomeIcon icon={faEnvelope} />
+                            </span>
+                            <Form.Control size="sm"  className="pl-5"  type="email" placeholder="Email" />
+                            
+                            
+                        </Form.Group>
+                        <Form.Group  className='mt-4' controlId="formBasicPassword">
+                            <span className='pl-3 pt-1 position-absolute' >
+                                <FontAwesomeIcon icon={faLock} />
+                            </span>
+                            <Form.Control size="sm"  className="pl-5"  type="password" placeholder="Password" />
+                            
+                            
+                        </Form.Group>
+                        <Form.Group  className='mt-4' controlId="formBasicCheckbox">
+                            <Form.Check   type="checkbox" label="Accept All terms & conditions" />
+                        </Form.Group>
+                        <Button size="sm"  className='mb-3 btn btn-primary btn-lg btn-block' variant="primary" type="submit">
+                            Create Account
+                        </Button>
+                        <br />
+                        
+                        Or <a href="#">get connected</a>
+                    </Form>
+                </Col>
+            </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default RegistrationPage;
