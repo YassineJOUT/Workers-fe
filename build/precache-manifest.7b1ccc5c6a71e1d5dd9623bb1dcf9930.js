8a1a19b4e5a5ee65217f319bfffc80be0e944b71
Yassine JOUT <yassinejout@gmail.com>
2020-03-08 22:42:37 +0100

    refacor(deployBranch)

---
+++ build/precache-manifest.7b1ccc5c6a71e1d5dd9623bb1dcf9930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c291c18ee65f09335272b0e64913c24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84aaec945b515800539",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e84aaec945b515800539",
+    "url": "/static/js/2.d3850b7c.chunk.js"
+  },
+  {
+    "revision": "de1b236c36118b848a30",
+    "url": "/static/js/main.2b3ebfca.chunk.js"
+  },
+  {
+    "revision": "649547da89c104d0f6ee",
+    "url": "/static/js/runtime-main.da328c51.js"
+  }
+]);
